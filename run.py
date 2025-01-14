4bb5a2647038c5cc85d74d77c0fdd3fceb3f66a0
Zakariye Farah Nor <0e6xrww7@students.codeinstitute.net>
2025-01-14 12:44:39 +0000

    add landing page

---
+++ run.py
@@ -0,0 +1,16 @@
+import os
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+#Landing page
+@app.route('/')
+def about():
+    return render_template("about.html")
+
+
+if __name__ == "__main__":
+    app.run(
+        host=os.environ.get("IP", "0.0.0.0"),
+        port=int(os.environ.get("PORT", "5000")),
+    debug=True)
