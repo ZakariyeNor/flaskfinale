8d7782af7366d15051582b6f28074408a824dfa1
Zakariye Farah Nor <0e6xrww7@students.codeinstitute.net>
2025-01-14 16:34:09 +0000

    add contact page

--- run.py
+++ run.py
@@ -1,8 +1,14 @@
 import os
 import json
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash
+if os.path.exists("env.py"):
+   import env
+
 
 app = Flask(__name__)
+app.secret_key = os.environ.get("SECRET_KEY")
+
+
 
 #Landing page
 @app.route('/')
@@ -11,6 +17,16 @@ def about():
     with open("data/company.json", "r") as json_data:
         data = json.load(json_data)
     return render_template("about.html", page_title = "About", company=data)
+
+
+#contact page
+@app.route('/contact', methods=["GET", "POST"])
+def contact():
+    if request.method == "POST":
+        flash("Thanks {}, we have received your message!".format(
+            request.form.get("name")))
+    return render_template('contact.html', page_title = "Contact")
+
 
 
 if __name__ == "__main__":
